a3fe6d2308ece37542029927be98fbdc4fa9bb05
KakeyamaY <ykakeyama3014@gmail.com>
2025-09-27 20:40:41 +0900

    Migrate to Nuxt (#2)

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  app: {
+    baseURL: process.env.PR_NUMBER ? `/pr-${process.env.PR_NUMBER}/` : '/',
+  }
+})

9e10bfd4c68de58767668098e38507ec94cd2333
yaito3014 <ykakeyama3014@gmail.com>
2025-09-27 20:19:08 +0900

    Enable preview deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    baseURL: process.env.PR_NUMBER ? `/pr-${process.env.PR_NUMBER}/` : '/',
+  }
 })
